e537bcec0d6b2155012f4be2fa73b6e6f601f30b
DamsDa <damarrasendriya@gmail.com>
2024-01-19 23:22:09 +0700

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,67 @@
+const App = Vue.createApp({
+  data() {
+    return {
+      state: true,
+      inputName: "",
+      names: [],
+      error: "",
+      showError: false,
+      result: "",
+    };
+  },
+  computed: {
+    isReady() {
+      return this.names.length > 1;
+    },
+  },
+  methods: {
+    addNameToList() {
+      const userName = this.inputName;
+      if (this.validate(userName)) {
+        this.names.push(userName);
+        this.inputName = "";
+        this.showError = false;
+        console.log(this.names);
+      } else {
+        this.showError = true;
+      }
+    },
+    validate(value) {
+      this.error = "";
+      if (value === "") {
+        this.error = "Input field cannot be empty";
+        return false;
+      }
+      if (this.names.includes(value)) {
+        this.error = "already in the list";
+        return false;
+      }
+      return true;
+    },
+    removeItemList(index) {
+      this.names.splice(index, 1);
+    },
+    showResult() {
+      this.generateResult();
+      this.state = false;
+    },
+    getRandomName() {
+      return this.names[Math.floor(Math.random() * this.names.length)];
+    },
+    generateResult() {
+      let randomName = this.getRandomName();
+      if(this.result !== ''){
+        while(randomName === this.result){
+            randomName = this.getRandomName();
+        }
+      }
+      this.result = randomName;
+    },
+    resetApp() {
+      (this.state = true), (this.inputName = ""), (this.names = []), (this.error = ""), (this.showError = false), (this.result = "");
+    },
+    getNewResult() {
+      this.generateResult();
+    },
+  },
+}).mount("#app");
